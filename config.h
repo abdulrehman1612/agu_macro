0850dd4d563f652ffd8b0c0aaa975f1dfe8b25de
abdulrehman1612 <138300432+abdulrehman1612@users.noreply.github.com>
2025-07-17 05:07:58 +0500

    Add files via upload

---
+++ config.h
@@ -0,0 +1,12 @@
+#define ENCODER_A_PINS { GP5, GP10  }
+#define ENCODER_B_PINS { GP6, GP9 }
+#define ENCODER_RESOLUTION 2
+#define ENCODER_MAP_KEY_DELAY 20
+
+#define WS2812_DI_PIN GP14              // Data pin you wired to LEDs         // Number of LEDs you have
+
+#define RGB_MATRIX_LED_COUNT 12
+#define DRIVER_LED_TOTAL 12
+#define WS2812_DI_PIN GP14
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100
+
